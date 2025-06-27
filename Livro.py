aacd0ffacb277a3acaa36efdc7c8db15e486d2a8
Sophia Maria <ibalunos1@gmail.com>
2025-06-27 16:03:39 -0200

    Adicionando
    Adicionando mais alguns métodos e atributos. Ainda irei finalizar

--- Livro.py
+++ Livro.py
@@ -4,10 +4,21 @@ class Livro:
         self.nome = nome
         self.genero = genero
         self.disponibilidade = disponibilidade
+        self.lista = []
 
+    def expirar_livro(self, genero, expiracao):
+        self.expiracao = expiracao
+        super().__init__(genero)
+        if genero == 'Escolar':
+
+    def salvar_livro(self,):
+        
 
 
 
 
     def __str__(self):
         return f"Livro:   {self.nome}, Autor: {self.autor}, Genero: {self.genero}"
+    
+#Precisa colocar a expiração no caso de livros escolares
+#tem que fazer uma condição em que: se o livro for escolar, tem que colocar a expiração. Se não for, não pedir.
