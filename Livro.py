faf24a8aa26cd14d06b5fe6c1b3dde9a31f12d6b
Sophia Maria <ibalunos1@gmail.com>
2025-06-27 19:20:40 -0300

    Fazendo o Método Expiração do Livro
    Tentando fazer a expiração do livro

--- Livro.py
+++ Livro.py
@@ -1,13 +1,23 @@
 class Livro:
-    def __init__(self, autor, nome, genero, disponibilidade):
+    def __init__(self, autor, nome, genero, expiracao):
         self.autor = autor
         self.nome = nome
         self.genero = genero
-        self.disponibilidade = disponibilidade
+        self.expiracao = expiracao
+      
 
-
-
-
-
+    def expirar(self, expiracao, genero):
+        if genero == "escolar":
+            return 'Digite a Expiração do livro'
+        else:
+           pass
+    
+    
     def __str__(self):
-        return f"Livro:   {self.nome}, Autor: {self.autor}, Genero: {self.genero}"
+        return f"Livro:   {self.nome}, Autor: {self.autor}, Genero: {self.genero}, Expiração : {self.expiracao}"
+    
+livro = Livro('1984', 'George Owell', 'Ficção', "001 ") 
+print(livro)  
+mensagem = livro.expirar(expiracao = "001 ")
+print(mensagem)
+#Fazer a Expiração do Livro, caso o Livro seja do genêro escolar
