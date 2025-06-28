2fa096a5a8d8cb94af3250825b93db112f636a30
Sophia Maria <ibalunos1@gmail.com>
2025-06-28 20:54:34 -0300

    Terminando o metodo expirar
    Ajeitei o metodo expirar caso o livro seja do genêro escolar

--- Livro.py
+++ Livro.py
@@ -1,23 +1,25 @@
 class Livro:
-    def __init__(self, autor, nome, genero, expiracao):
+    def __init__(self, autor, nome, genero, expiracao=''):
+        #Expiração do Livro
+        if genero == 'escolar' and not expiracao:
+            raise ValueError("O livro é escolar porém não foi colocado uma expiração")
+        
         self.autor = autor
         self.nome = nome
         self.genero = genero
-        self.expiracao = expiracao
+        self.expiracao
+
+        
       
 
-    def expirar(self, expiracao, genero):
-        if genero == "escolar":
-            return 'Digite a Expiração do livro'
-        else:
-           pass
+    
     
     
     def __str__(self):
-        return f"Livro:   {self.nome}, Autor: {self.autor}, Genero: {self.genero}, Expiração : {self.expiracao}"
+        return f"Livro:{self.nome}, Autor: {self.autor}, Genero: {self.genero}, Expiração : {self.expiracao}"
     
-livro = Livro('1984', 'George Owell', 'Ficção', "001 ") 
-print(livro)  
-mensagem = livro.expirar(expiracao = "001 ")
-print(mensagem)
+#livro = Livro('1984', 'George Owell', 'Ficção', " ") 
+#print(livro)  
+#mensagem = livro.expirar(expiracao = "001 ")
+#print(mensagem)
 #Fazer a Expiração do Livro, caso o Livro seja do genêro escolar
