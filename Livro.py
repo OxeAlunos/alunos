a74f80d9e8162ba60ee36fb5a6951123fad9647b
Sophia Maria <ibalunos1@gmail.com>
2025-06-29 12:14:32 -0300

    ISBN
    adicionei o método para a ISBN do livro.

--- Livro.py
+++ Livro.py
@@ -1,5 +1,5 @@
 class Livro:
-    def __init__(self, autor, nome, genero, disponibilidade, expiracao=''):
+    def __init__(self, autor, nome, genero, disponibilidade, isbn, expiracao=''):
         #Expiração do Livro
         if genero == 'escolar' and not expiracao:
             raise ValueError("O livro é escolar porém não foi colocado uma expiração")
@@ -7,8 +7,21 @@ class Livro:
         self.autor = autor
         self.nome = nome
         self.genero = genero
-        self.expiracao
+        self.expiracao = expiracao
         self.disponivel = disponibilidade
+        self.isbn = isbn
+
+        #Isbn do Livro
+        string_isbn = str(isbn)
+        tamanho = len(string_isbn)
+        if isbn == 'não tem':
+            pass
+        elif isbn != 'não tem' and tamanho < 13:
+            return ValueError("O ISBN está errado e/ou imcompleto")
+        else:
+            pass
+
+
 
         
       
@@ -17,10 +30,13 @@ class Livro:
     
     
     def __str__(self):
-        return f"Livro:{self.nome}, Autor: {self.autor}, Genero: {self.genero}, Expiração : {self.expiracao}"
+        return f"Livro:{self.nome}, Autor: {self.autor}, Genero: {self.genero},ISBN: {self.isbn} Expiração : {self.expiracao}"
     
 #livro = Livro('1984', 'George Owell', 'Ficção', " ") 
 #print(livro)  
 #mensagem = livro.expirar(expiracao = "001 ")
 #print(mensagem)
 #Fazer a Expiração do Livro, caso o Livro seja do genêro escolar
+#O isbn não é obrigatório, neste tópico ele se comporta que nem a expiração
+#Fazer uma verificação da quantidade de caracteres (tem que ser 13 caracteres)
+#o isbn vai ser do tipo int e para a verificação fazer a conversão para str
