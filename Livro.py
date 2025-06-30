8eba7eb3a52e6e9bfa4aac7144a6c3535cfdec04
Sophia Maria <ibalunos1@gmail.com>
2025-06-30 20:30:24 -0300

    Fiz alguns comentários
    Fiz comentários explicando algumas coisas sobre o ISBN.

--- Livro.py
+++ Livro.py
@@ -12,10 +12,11 @@ class Livro:
         self.disponivel = disponibilidade
         self.isbn = isbn
 
-        #Isbn do Livro
+        #Livro ISBN, ISBN = int e string_isbn = string, converte int(isbn) para string(string_isbn)
         string_isbn = str(isbn)
         tamanho = len(string_isbn)
         if isbn == '':
+            print(f'O ISbn é: {string_isbn}')
             pass
         elif isbn != '' and tamanho < 13:
             raise ValueError("O ISBN está errado e/ou imcompleto")
@@ -23,6 +24,8 @@ class Livro:
             pass
         else:
             raise ValueError("O ISBN está errado e/ou imcompleto")
+        
+        
 
         
 
@@ -36,8 +39,11 @@ class Livro:
     def __str__(self):
         return f"Livro:{self.nome}, Autor: {self.autor}, Genero: {self.genero},ISBN: {self.isbn}, Expiração : {self.expiracao}"
     
-livro = Livro('1984', 'George Owell', 'Ficção', '', '') 
-print(livro)  
+livro = Livro('1984', 'George Owell', 'Ficção', 1234567890123
+ , '') 
+#livro.verifcar_caracter()
+print(livro.isbn)
+  
 
 #Fazer a Expiração do Livro, caso o Livro seja do genêro escolar
 #O isbn não é obrigatório, neste tópico ele se comporta que nem a expiração
