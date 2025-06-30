d2878f718a6fe9df57bc43faf424f018523433ef
Sophia Maria <ibalunos1@gmail.com>
2025-06-30 16:39:30 -0300

    Melhorando o ISBN
    Revisei a parte do ISBN e deixei ele funcionado de uma maneira melhor e correta.

--- Livro.py
+++ Livro.py
@@ -1,8 +1,9 @@
 class Livro:
-    def __init__(self, autor, nome, genero, disponibilidade, isbn='não tem', expiracao=''):
+    def __init__(self, autor, nome, genero, isbn, disponibilidade, expiracao=''):
         #Expiração do Livro
         if genero == 'escolar' and not expiracao:
             raise ValueError("O livro é escolar porém não foi colocado uma expiração")
+        
         
         self.autor = autor
         self.nome = nome
@@ -14,13 +15,16 @@ class Livro:
         #Isbn do Livro
         string_isbn = str(isbn)
         tamanho = len(string_isbn)
-        if isbn == 'não tem':
+        if isbn == '':
             pass
-        elif isbn != 'não tem' and tamanho < 13:
-            return ValueError("O ISBN está errado e/ou imcompleto")
+        elif isbn != '' and tamanho < 13:
+            raise ValueError("O ISBN está errado e/ou imcompleto")
+        elif isbn != '' and tamanho == 13:
+            pass
         else:
-            pass
+            raise ValueError("O ISBN está errado e/ou imcompleto")
 
+        
 
 
         
@@ -30,12 +34,11 @@ class Livro:
     
     
     def __str__(self):
-        return f"Livro:{self.nome}, Autor: {self.autor}, Genero: {self.genero},ISBN: {self.isbn} Expiração : {self.expiracao}"
+        return f"Livro:{self.nome}, Autor: {self.autor}, Genero: {self.genero},ISBN: {self.isbn}, Expiração : {self.expiracao}"
     
-#livro = Livro('1984', 'George Owell', 'Ficção', " ") 
-#print(livro)  
-#mensagem = livro.expirar(expiracao = "001 ")
-#print(mensagem)
+livro = Livro('1984', 'George Owell', 'Ficção', '', '') 
+print(livro)  
+
 #Fazer a Expiração do Livro, caso o Livro seja do genêro escolar
 #O isbn não é obrigatório, neste tópico ele se comporta que nem a expiração
 #Fazer uma verificação da quantidade de caracteres (tem que ser 13 caracteres)
